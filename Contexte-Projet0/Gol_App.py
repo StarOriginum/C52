a50c94c1ee3785ecc1bf00f9b72ef103392e408a
Michael Turner <michaelturner.affaires@hotmail.com>
2024-09-23 15:08:53 -0400

    Travail ajout des Widgets

--- Contexte-Projet0/Gol_App.py
+++ Contexte-Projet0/Gol_App.py
@@ -16,9 +16,11 @@ from __feature__ import snake_case, true_property
 
 
 class QInformationPanel(QWidget):
-    def __init__(self, parent = None):
+    def __init__(self, parent = None, gol_apps = None, gol_eng = None):
         super().__init__(None)
         
+        self.__gol_app = gol_apps
+        self.__gol_engine = gol_eng
         self.__generations = 0
         self.__alive = 0
         self.__dead = 0
@@ -41,11 +43,12 @@ class QInformationPanel(QWidget):
     def update_stats(self):
 
         self.__generations += 1
+        
+        
+        self.__alive = sum(sum(row) for row in self.__gol_engine.get_grid())
+        self.__dead = (self.__gol_engine.width * self.__gol_engine.height) - self.__alive
 
-        self.__alive = sum(sum(row) for row in self.__gol.get_grid())
-        self.__dead = (self.__gol.width * self.__gol.height) - self.__alive
-
-        self.__total_cells = self.__gol.width * self.__gol.height
+        self.__total_cells = self.__gol_engine.width * self.__gol_engine.height
         self.__alive_percent = (self.__alive / self.__total_cells) * 100
         self.__dead_percent =  (self.__dead / self.__total_cells) * 100
 
@@ -71,6 +74,32 @@ class QControlPanels(QWidget):
     def __init__(self, parent = None):
         super().__init__(None)
         
+        self.__start_pause_button = QPushButton("Start")
+        self.__start_pause_button.clicked.connect(self.toggle_simulation)
+        
+        self.__step_button = QPushButton("Step")
+        self.__step_button.clicked.connect(self.step_simulation)
+        
+        self.__speed_scrollbar = QScrollBar(Qt.Horizontal)
+        self.__speed_scrollbar.valueChanged.connect(self.change_simulation_speed)
+        
+        self.__controls_layout = QBoxLayout(QBoxLayout.LeftToRight)
+        self.__controls_layout.add_widget(self.__start_pause_button)
+        self.__controls_layout.add_widget(self.__step_button)
+        self.__controls_layout.add_widget(self.__speed_scrollbar)
+        
+        self.set_layout(self.__controls_layout)
+        
+    def toggle_simulation(self):
+        self.toggle_simulation_signal.emit(self.__start_pause_button)
+    
+    def step_simulation(self):
+        self.step_simulation_signal.emit(self.__step_button)
+        
+    def change_simulation_speed(self):
+        self.speed_changed_signal.emit(self.__speed_scrollbar)
+        
+        
         
         
         
@@ -88,26 +117,36 @@ class GolApp(QMainWindow):
         self.set_window_title('GolApp')
         self.__height = 250
         self.__width = 200
+       
+        
+        
+        self.__gol = GOLEngine(self.__height, self.__width)
+        self.__gol.randomize()
+        
+        self.__info_panel = QInformationPanel(gol_apps=self, gol_eng=self.__gol)
+        self.__control_panel = QControlPanels()
 
 
         self.__simulation_running = True
         self.__app_view = QLabel()
-        
-        self.__info_panel = QInformationPanel()
         self.__right_box = QGroupBox("Stats")
-        self.__right_box.set_layout(QVBoxLayout())
+        self.__right_box.set_layout(QVBoxLayout(QBoxLayout.BottomToTop))
         self.__right_box.layout().add_widget(self.__info_panel)
         # self.__stats_widget = QWidget(self)
         # self.__stats_widget.set_layout()
         self.__right_layout = QVBoxLayout()
         self.__right_layout.add_widget(self.__right_box)
+        
+        self.__left_top_box = QGroupBox("Controls")
+        self.__left_top_box.set_layout(QVBoxLayout())
+        self.__left_top_box.layout().add_widget(self.__button_panel)
+        
+        self.__left_layout = QVBoxLayout(QBoxLayout.TopToBottom)
+        self.__left_layout.add_widget(self.__left_top_box)
 
-        self.__simulation_button = QPushButton("Stop")
-        self.__simulation_button.clicked.connect(self.toggle_simulation)
-
-        self.__button_layout = QBoxLayout(QBoxLayout.LeftToRight)
-        self.__button_layout.add_widget(self.__simulation_button)
-
+        self.__control_panel.toggle_simulation_signal.connect(self.toggle_simulation)
+        self.__control_panel.step_simulation_signal.connect(self.step_simulation)
+        self.__control_panel.speed_changed_signal.connect(self.change_simulation_speed)
 
         self.__main_layout = QBoxLayout(QBoxLayout.LeftToRight)
         self.__main_layout.add_widget(self.__simulation_button)
@@ -118,8 +157,7 @@ class GolApp(QMainWindow):
         self.__central_widget.set_layout(self.__main_layout)
         self.set_central_widget(self.__central_widget)
 
-        self.__gol = GOLEngine(self.__height, self.__width)
-        self.__gol.randomize()      
+              
         
         self.__app_view.alignment = Qt.AlignCenter
         
@@ -153,8 +191,9 @@ class GolApp(QMainWindow):
         pixmap = QPixmap.from_image(self.__image)
         
         self.__app_view.pixmap = pixmap
-        self.update_stats()
-        
+        self.__info_panel.update_stats()
+     
+    @Slot    
     def toggle_simulation(self):
         if self.__simulation_running:
             self.__timer.stop()
